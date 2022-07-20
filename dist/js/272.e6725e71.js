6b9a98fda457de3ad966fcc7bd8e7cd1d6864dfc
Zeus Moreira <zeus.pereira@azi.com.br>
2022-07-20 03:01:56 -0400

    Adicionando a pasta dist ao repositorio

---
+++ dist/js/272.e6725e71.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_todo"]=self["webpackChunkvue_todo"]||[]).push([[272],{1272:function(t,u,n){n.r(u),n.d(u,{default:function(){return i}});var e=function(){var t=this;t._self._c;return t._m(0)},s=[function(){var t=this,u=t._self._c;return u("div",{staticClass:"about"},[u("h1",[t._v("This is an about page")])])}],a=n(1001),l={},r=(0,a.Z)(l,e,s,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=272.e6725e71.js.map
